44630d36896b4bb3e3779858de83e03d8092027a
Tnaike <timmynaike@gmail.com>
2023-08-19 23:50:24 +0100

    Add home and footer

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import ejs from 'ejs';
+
+const homeStartingContent = 'Welcome to our home page';
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', ejs);
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('form.ejs');
+  res.render('home', { startingContent: homeStartingContent });
 });
 
 app.listen(port, () => {
